74e5a151702276934bd11414eb086fd8db134fb5
Sucifitria Azhara <2522500057@mahasiswa.atmaluhur.ac.id>
2026-09-21 17:42:23 +0700

    create indlocalhost/dpwl_nama_123

---
+++ Pertemuan-02/Latihan1Controller.php
@@ -0,0 +1,12 @@
+<?php
+require_once 'model/Latihan1Model.php';
+
+class Latihan1Controller
+{
+    public function index()
+    {
+        $model = new Latihan1Model();
+        $dataMhs = $model->getAllMhs();
+        require './view/latihan1view.php';
+    }
+}
---
+++ Pertemuan-02/Latihan1Model.php
@@ -0,0 +1,12 @@
+<?php
+class Latihan1Model
+{
+    public function getAllMhs()
+    {
+        return [
+            ['nama' => 'Fakhril', 'nim' => '3022501'],
+            ['nama' => 'Rani', 'nim' => '3022502'],
+            ['nama' => 'Dresta', 'nim' => '3022503']
+        ];
+    }
+}
